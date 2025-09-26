d59a7e49df8a3e14f69bdc4f1e2722c5d83b9803
dinamitrii <k_karaivanov@abv.bg>
2025-09-26 16:28:14 +0300

    commited

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 import base64
 import json
+from pprint import pprint
 import folium
 from folium import IFrame, ClickForMarker
 from folium.plugins import FloatImage, Geocoder
@@ -9,6 +11,7 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, url_for
 
 load_dotenv(verbose=True)
+
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
 
@@ -27,23 +30,48 @@ def index():
 
     print(json_data)
 
+    # weather_url=f"https://api.openweathermap.org/data/2.5/fwi?lat={json_data['lat']}&lon={json_data['lon']}&appid={os.getenv('API_KEY')}"
+    #
+    # weather_data = requests.get(weather_url).text
+    #
+    # weather_json = json.loads(weather_data)
+    #
+    #
+    # pprint(weather_json)
+    #
+    #
+    #
+    #
+    #
     geolocator = Nominatim(timeout=10, user_agent="Dinamitrii")
 
+    loc =  geolocator.geocode(json_data)
 
-    maps_to = folium.Map(location=[json_data['lat'], json_data['lon']], zoom_start=12, tiles='OpenStreetMap',zoom_control="bottomleft")
+    print(loc)
 
-    maps_to.add_child(Geocoder(), name='Geocoder').get_bounds()
+    address = loc.address
 
-    # folium.GeoJson(location=[json_data['lat'], json_data['lon']], name='walk_to').add_to(maps_to)
+    print(address)
+
+
+
+    maps_to = folium.Map([loc.latitude, loc.longitude], tiles='OpenStreetMap', zoom_start=16, zoom_control="bottomleft")
 
     tooltip = "Click to see @"
     html = '<img src="data:image/png;base64,{}">'.format
     picture1 = base64.b64encode(open('static/images/xtras/1.png', 'rb').read()).decode()
-    iframe1 = IFrame(html(picture1), '300','300')
+    picture2 = base64.b64encode(open('static/images/xtras/12.png', 'rb').read()).decode()
+    iframe1 = IFrame(html(picture1), '300', '300')
+    iframe2 = IFrame(html(picture2), '300', '300')
     popup1 = folium.Popup(iframe1, max_width=300)
-    icon1 = folium.Icon(color111111111111111111111111111111111111111111111111111="blue", icon="info-sign")
+    popup2 = folium.Popup(iframe2, max_width=300)
+    icon1 = folium.Icon(color="blue", icon="info-sign")
+    icon2 = folium.Icon(color="green", icon="info-sign")
 
-    folium.Marker(location=[json_data['lat'], json_data['lon']], popup=popup1, tooltip=tooltip, icon=icon1).add_to(maps_to)
+
+    folium.Marker(location=[loc.latitude, loc.longitude],popup=loc.address, tooltip=tooltip, icon=icon1).add_to(maps_to)
+
+    folium.plugins.Geocoder(loc.address).add_to(maps_to)
 
     url = (
         "https://raw.githubusercontent.com/ocefpaf/secoora_assets_map/"
@@ -52,12 +80,7 @@ def index():
 
     FloatImage(url, bottom=60, left=30).add_to(maps_to)
 
-
-
-
     maps_to.add_child(ClickForMarker())
-
-
 
     context = {
 
@@ -83,7 +106,6 @@ def favicon():
             url_for('static', filename='images/favicon/mstile-150x150.png'),
             url_for('static', filename='images/favicon/browserconfig.xml8lr'),
             url_for('static', filename='images/favicon/site.webmanifest'))
-
 
 
 if __name__ == '__main__':
---
+++ player.py
@@ -0,0 +1,10 @@
+import m3u8
+import playwright.sync_api
+
+playlist = m3u8.load('https://lon.rtsp.me/DjAvotRO05KzedNZjUSbOg/1758886128/hls/4NSnNeAb.m3u8')
+
+playlist.
+
+
+
+print(playlist.dumps())
